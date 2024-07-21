cca5104c055746a4f35bd48d7fa6d0e2162091f7
GURU VIJAY M <144215156+Guru12e@users.noreply.github.com>
2024-07-21 18:12:25 +0530

    Update page.tsx

--- app/(root)/wishList/page.tsx
+++ app/(root)/wishList/page.tsx
@@ -9,7 +9,7 @@ const WishList = () => {
 
   return (
     <div>
-      <h1 className='uppercase text-center text-2xl mt-4 font-bold'>
+      <h1 className='uppercase text-center text-2xl mt-4 font-bold mb-4'>
         WishList
       </h1>
       <div className='w-4/5 mx-auto mt-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-12'>
